7393d33c483315b028b466e223e49c4519ceb42a
Naqhi <naqhi2001@gmail.com>
2023-10-25 11:34:00 +0530

    added

---
+++ example.js
@@ -0,0 +1,25 @@
+//   function getNumber(n){
+//     if(n==='one'){
+//         document.querySelector('.id1').textContent=n;
+//     }else if(n==='two'){
+//         document.querySelector('.id2').textContent=n;
+//     }else if(n==='three'){
+//         document.querySelector('.id3').textContent=n;
+//     }else if(n==='four'){
+//         document.querySelector('.id4').textContent=n;
+//     }
+//     console.log( n);
+//   }
+function changeThis(n){
+if(n==='red'){
+    document.body.style.backgroundColor = n;
+}else if(n==='blue'){
+    document.body.style.backgroundColor= n;
+    
+}else if( n==='green'){
+    document.body.style.backgroundColor= n;
+
+}else if(n==='yellow'){
+    document.body.style.backgroundColor= n;
+}
+}
